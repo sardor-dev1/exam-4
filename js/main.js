7db116562ffab26331e9175e3f39876ba363f578
sardor-dev1 <sardorprogramist@gmail.com>
2024-04-30 22:20:43 +0500

    Examp Exclusive wep-site

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let menu = document.querySelector(".header__hamburger");
+let header = document.querySelector(".header");
+let navBar = document.querySelector(".header__list");
+let Close = document.querySelector(".header__item--close");
+let backTop = document.querySelector(".back-top");
+
+window.addEventListener("scroll", function () {
+  showBackTop();
+  showHeaderShrink();
+});
+
+menu.addEventListener("click", function () {
+  navBar.classList.toggle("header__show__list");
+});
+Close.addEventListener("click", function () {
+  navBar.classList.remove("header__show__list");
+});
+
+function showBackTop() {
+  if (scrollY > 100) {
+    backTop.classList.add("show__back-top");
+  } else {
+    backTop.classList.remove("show__back-top");
+  }
+}
+function showHeaderShrink() {
+  if (scrollY > 0) {
+    header.classList.add("show__header__shrink");
+  } else {
+    header.classList.remove("show__header__shrink");
+  }
+}
