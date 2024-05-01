42c6411b95830eb9878f93f11539f7bc1ba5a8a8
sardor-dev1 <sardorprogramist@gmail.com>
2024-05-01 05:50:33 +0500

    added product cards responsive

--- js/app.js
+++ js/app.js
@@ -1,35 +1,37 @@
 const API_URL = "https://fakestoreapi.com";
 
-const productsCards = document.querySelector(".products")
+const productsCards = document.querySelector(".products");
 
-document.addEventListener("DOMContentLoaded", ()=> {
-    loadProductData(API_URL)
-})
-
+document.addEventListener("DOMContentLoaded", () => {
+  loadProductData(API_URL);
+});
 
 async function loadProductData(api) {
-    let reponse = await fetch(`${api}/products`)
-    reponse
-      .json()
-      .then((res) => renderProducts(res))
-      .catch((err) => console.log(err));
+  let reponse = await fetch(`${api}/products?limit=8`);
+  reponse
+    .json()
+    .then((res) => renderProducts(res))
+    .catch((err) => console.log(err));
 }
 
 // loadProductData(API_URL)
 
 function renderProducts(data) {
-    let cards = ""
+  let cards = "";
 
-    data.forEach(product => {
-        cards += `
+  data.forEach((product) => {
+    cards += `
         <div class="product__card">
-            <img class="product__card__image" src=${product.image} alt="">
+            <div class="product__card__image">
+                <img src=${product.image} alt="">
+                <button class="product__card__image__btn">Add To Cart</button>
+            </div>
             <div class="product__card__info">
               <h3 class="product__card__info__title">${product.category}</h3>
-              <p class="product__card__info__text">${product.price}</p>
+              <p class="product__card__info__text">${product.price}$ (95)</p>
             </div>
           </div>
         `;
-    });
-    productsCards.innerHTML = cards
+  });
+  productsCards.innerHTML = cards;
 }
